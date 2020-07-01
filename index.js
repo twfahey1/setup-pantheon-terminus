77ab3deb50cf8cdf8581741a625236a7db040e15
Tyler Fahey <twfahey1@gmail.com>
2020-06-30 21:22:17 -0500

    Revise to use composer installation

--- index.js
+++ index.js
@@ -12,8 +12,8 @@ async function run() {
 	try {
 		const PANTHEON_MACHINE_TOKEN = core.getInput( 'pantheon-machine-token' );
 
-		await exec.exec( 'curl -O https://raw.githubusercontent.com/pantheon-systems/terminus-installer/master/builds/installer.phar' );
-		await exec.exec( 'sudo php installer.phar install' ); // Sudo is required in order to install bin/terminus.
+		await exec.exec( 'mkdir ~/terminus && cd ~/terminus && composer require pantheon-systems/terminus' );
+		await exec.exec( 'sudo ln -s ~/terminus/vendor/bin/terminus /usr/bin/terminus' ); // Sudo is required in order to install bin/terminus.
 		await exec.exec( 'terminus', [ 'auth:login', `--machine-token=${ PANTHEON_MACHINE_TOKEN }` ] );
 	} catch ( error ) {
 		core.setFailed( error.message );
